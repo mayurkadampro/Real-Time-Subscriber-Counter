bf8420830096f5dd5b0e929e5e10c84fe4c3f263
mayurkadam200 <mayurkadam200@gmail.com>
2018-11-09 10:21:37 +0530

    update

--- src/youtube_live/YouTube_Live.java
+++ src/youtube_live/YouTube_Live.java
@@ -1,7 +1,6 @@
 package youtube_live;
 
 /*
-
 Author : Mighty Ghost Hack | www.youtube.com/c/mightyghosthack
 Licence : GPL v3 or any later version
 
@@ -15,19 +14,17 @@ but WITHOUT ANY WARRANTY;
 See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
  */
 import java.awt.Color;
-import java.awt.Cursor;
 import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.RoundRectangle2D;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -40,8 +37,12 @@ import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JWindow;
 import javax.swing.SwingConstants;
@@ -50,18 +51,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class YouTube_Live extends Thread {
-    
+
     private JSONObject myResponse1;
     private JSONObject myResponse2;
     private long subscribe;
     private final JLabel label1;
     private final JLabel label3;
-    private static final String IMG_PATH = "src/image/cut.png";
+    private final JLabel label4;
+    private final JLabel label5;
+    private final JLabel label6;
     private final JWindow win;
     private final String cut = "X";
+    private final String refresh = "X";
+    private final String comment = "";
     private final String id = "UCeVmancWx92vTZ9IPYOKnKg"; //paste channel id here
-    private final String Username = "Mighty Ghost Hack";
-    private final String APIKey = "AIzaSyD4a5qZCNfXIq7j0EcOB6pbonCY7eeEyFg";
+    private final String Username = "Mighty Ghost Hack"; //channel Name
+    private final String APIKey = "AIzaSyD4a5qZCNfXIq7j0EcOB6pbonCY7eeEyFg"; //Your API Key
     String URL = "https://www.youtube.com/channel/UCeVmancWx92vTZ9IPYOKnKg"; //channel url
     private final JLabel label2;
     private NumberFormat nf;
@@ -75,10 +80,17 @@ public class YouTube_Live extends Thread {
     double new_sub;
     double new_sub1;
     private int mx, my;
-    
+    private int countforlink = 0;
+    private int countformusic = 0;
+    private double subscribeMileStoneAlert = 12000.0; // you need to set the milestone base on your subscribe
+    private boolean milestone_update = true;
+    private boolean UpdateSubWhenStart = true;
+    private double subscriberUpdateWhenStart;
+    private double subscriberUpdateWhenStartforminus;
+
     public void Call_me() throws Exception {
         nf = NumberFormat.getInstance(Locale.ENGLISH);
-        
+
         Thread thread = new Thread() {
             public void run() {
                 try {
@@ -99,21 +111,21 @@ public class YouTube_Live extends Thread {
                                 response.append(inputLine);
                             }
                         }
-                        
+
                         JSONObject myResponse = new JSONObject(response.toString());
                         JSONArray jsonarray = new JSONArray(myResponse.getJSONArray("items").toString());
-                        
+
                         for (int i = 0; i < jsonarray.length(); i++) {
-                            
+
                             JSONObject jsonobject = jsonarray.getJSONObject(i);
                             JSONObject myResponse1 = new JSONObject(jsonobject.toString(i));
                             JSONObject myResponse2 = new JSONObject(jsonobject.getJSONObject("statistics").toString());
                             double subscribe = Integer.parseInt(myResponse2.getString("subscriberCount"));
                             //below inside of if condition it's minus the subscriber only in the begining 
-                            //only for show the counting........ so it's look similalr to socialblade
+                            //only for show the counting........ so it's look similar to socialblade
                             if (past_update) {
                                 double new_sub = subscribe - 10;
-                                System.out.println(new_sub);
+                                //System.out.println(new_sub);
                                 label1.setText(nf.format(new_sub));
                                 for (int j = 0; j < 10; j++) {
                                     new_sub1 = new_sub + j;
@@ -123,41 +135,116 @@ public class YouTube_Live extends Thread {
                                     label1.setSize(200, 80);
                                     label1.setLocation(width / 2 - 80, height / 2 + 40);
                                     label1.setFont(new Font("Roboto", Font.BOLD, 28));
+                                    past_update = false;
                                 }
                             }
+
+                            if (UpdateSubWhenStart) {
+                                subscriberUpdateWhenStart = subscribe;
+                                subscriberUpdateWhenStart += 1;
+                                UpdateSubWhenStart = false;
+                            }
+
+                            if (subscribe == subscriberUpdateWhenStart) {
+                                subscriberUpdateWhenStart += 1;
+                                if(countformusic % 2 == 0){
+                                    playAlertOnEachSubscribe();
+                                }
+                                subscriberUpdateWhenStartforminus = subscribe;
+                                subscriberUpdateWhenStartforminus -= 1;
+
+                            } else if (subscribe == subscriberUpdateWhenStartforminus) {
+                                subscriberUpdateWhenStartforminus -= 1;
+                                if(countformusic % 2 == 0){
+                                    playAlertForLostSub();
+                                }
+                                subscriberUpdateWhenStart = subscribe;
+                                subscriberUpdateWhenStart += 1;
+                            } else {
+                                subscriberUpdateWhenStart = subscribe;
+                                subscriberUpdateWhenStart += 1;
+                                subscriberUpdateWhenStartforminus = subscribe;
+                                subscriberUpdateWhenStartforminus -= 1;
+
+                            }
+
+                            if (milestone_update) {
+                                if (subscribe == subscribeMileStoneAlert) {
+                                    playAlertOnReachMilestone();
+                                    milestone_update = false;
+                                }
+
+                            }
+
                             label1.setText(nf.format(subscribe));
                             label1.setSize(200, 80);
                             label1.setLocation(width / 2 - 80, height / 2 + 40);
                             label1.setFont(new Font("Roboto", Font.BOLD, 28));
-                            past_update = false;
                         }
                     }
                 } catch (MalformedURLException ex) {
                     Logger.getLogger(YouTube_Live.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    Logger.getLogger(YouTube_Live.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (JSONException ex) {
-                    Logger.getLogger(YouTube_Live.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (InterruptedException ex) {
+                } catch (IOException | JSONException | InterruptedException ex) {
                     Logger.getLogger(YouTube_Live.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                
+
             }
         };
         thread.start();
-        
+
     }
-    
+
     public YouTube_Live() throws Exception {
-        
+
         win = new JWindow();
+        label5 = new JLabel(comment);
+        label5.setForeground(new Color(238, 238, 238));
+        label5.setSize(50, 20);
+        label5.setFont(new Font("Roboto", Font.BOLD, 10));
+        label5.setLocation(width / 2, height / 2 - 11);
+
+        label4 = new JLabel(refresh);
+
+        label4.setSize(50, 20);
+        label4.setForeground(new Color(238, 238, 238));
+        label4.setFont(new Font("Roboto", Font.BOLD, 35));
+        //JOptionPane.showMessageDialog(null, label4);
+        label4.setLocation(width / 2 - 180, height / 2 - 10);
+        label4.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent me) {
+                label4.setForeground(new Color(179, 56, 44));
+                label5.setText("Refresh");
+                label5.setForeground(new Color(179, 56, 44));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+                label4.setForeground(new Color(238, 238, 238));
+                label5.setText("");
+                label5.setForeground(new Color(238, 238, 238));
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent me) {
+                try {
+                    YouTube_Live yt = new YouTube_Live();
+                    yt.Call_me();
+                } catch (Exception ex) {
+                    Logger.getLogger(YouTube_Live.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        });
+
         label3 = new JLabel(cut);
         label3.setForeground(new Color(238, 238, 238));
-        label3.setSize(300, 20);
+        label3.setSize(50, 20);
         label3.setLocation(width / 2 + 178, height / 2 - 10);
         label3.setFont(new Font("Roboto", Font.BOLD, 35));
-        //JOptionPane.showMessageDialog(null, label3);
+
         label1 = new JLabel(String.valueOf(new_sub), SwingConstants.CENTER);
+
         label2 = new JLabel(Username, SwingConstants.CENTER);
         label2.setForeground(new Color(179, 56, 44));
         label2.setSize(350, 50);
@@ -166,6 +253,13 @@ public class YouTube_Live extends Thread {
         label2.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
+                if (countforlink % 2 == 0) {
+                    countforlink++;
+                    label2.setForeground(new Color(66, 98, 244));
+                } else {
+                    countforlink++;
+                    label2.setForeground(new Color(179, 56, 44));
+                }
                 if (e.getClickCount() > 0) {
                     if (Desktop.isDesktopSupported()) {
                         Desktop desktop = Desktop.getDesktop();
@@ -177,58 +271,66 @@ public class YouTube_Live extends Thread {
                     }
                 }
             }
-        }
-        );
-        
+        });
+
         label3.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
                 label3.setForeground(new Color(179, 56, 44));
+                label5.setText("Close");
+                label5.setForeground(new Color(179, 56, 44));
             }
-            
+
             @Override
             public void mouseExited(MouseEvent me) {
                 label3.setForeground(new Color(238, 238, 238));
+                label5.setText("");
+                label5.setForeground(new Color(238, 238, 238));
             }
-            
+
             @Override
             public void mouseClicked(MouseEvent e) {
                 System.exit(0);
             }
-            
+
         });
-        
-         /*
-        //draggging the jwindow code
-        win.addMouseListener(new MouseAdapter() {
-            
+
+        label5.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseMoved(MouseEvent e) {
-                mx = e.getXOnScreen();
-                my = e.getYOnScreen();
-                
+            public void mouseEntered(MouseEvent me) {
+                label5.setForeground(new Color(179, 56, 44));
             }
-            
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                Point p = win.getLocation();
-                p.x += e.getXOnScreen() - mx;
-                p.y += e.getYOnScreen() - my;
-                System.out.println(p.x);
-                System.out.println(p.y);
-                mx = e.getXOnScreen();
-                my = e.getYOnScreen();
-                win.setLocation(p);
-                
-            }
-            
+
         });
-        
-        */
-        
-        
+
+        ImageIcon img = new ImageIcon("C:\\Users\\Mayur\\Documents\\NetBeansProjects\\YouTube_Live\\src\\Images\\refresh.png");
+        ImageIcon img1 = new ImageIcon("C:\\Users\\Mayur\\Documents\\NetBeansProjects\\YouTube_Live\\src\\Images\\mute.png");
+        label6 = new JLabel(img);
+        label6.setBackground(new Color(0, 0, 0, 0));
+        label6.setSize(50, 50);
+        label6.setLocation(width / 2 + 161, height / 2 + 60);
+
+        label6.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent me) {
+                if (countformusic % 2 == 0) {
+                    label6.setIcon(img1);
+                    countformusic++;
+                } else {
+                    label6.setIcon(img);
+                    countformusic++;
+                    
+                }
+
+            }
+
+        });
+
+        win.add(label6);
+        win.add(label4);
         win.add(label3);
         win.add(label2);
+        win.add(label5);
         win.add(label1);
         win.pack();
         win.setShape(new RoundRectangle2D.Double(width / 2 - 190, height / 2 - 20, width / 2, 130, 50, 50));
@@ -236,11 +338,53 @@ public class YouTube_Live extends Thread {
         win.setSize(width, height);
         win.setLocation(screenWidth - 620, screenHeight - 1225);
         win.setVisible(true);
+
     }
-    
+
+    public void playAlertOnReachMilestone() {
+        try {
+            File sound = new File("C:\\Users\\Mayur\\Documents\\NetBeansProjects\\YouTube_Live\\src\\Sound\\beep-07.wav");
+            AudioInputStream audio = null;
+            audio = AudioSystem.getAudioInputStream(sound);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audio);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            Logger.getLogger(YouTube_Live.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void playAlertOnEachSubscribe() {
+        try {
+            File sound = new File("C:\\Users\\Mayur\\Documents\\NetBeansProjects\\YouTube_Live\\src\\Sound\\coin.wav");
+            AudioInputStream audio = null;
+            audio = AudioSystem.getAudioInputStream(sound);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audio);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            Logger.getLogger(YouTube_Live.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void playAlertForLostSub() {
+        try {
+            File sound = new File("C:\\Users\\Mayur\\Documents\\NetBeansProjects\\YouTube_Live\\src\\Sound\\lost.wav");
+            AudioInputStream audio = null;
+            audio = AudioSystem.getAudioInputStream(sound);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audio);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            Logger.getLogger(YouTube_Live.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
     public static void main(String[] args) throws Exception {
         YouTube_Live yt = new YouTube_Live();
         yt.Call_me();
     }
-    
+
 }
